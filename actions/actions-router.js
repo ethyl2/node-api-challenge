4159ffe48b3ab740d8ea8f09b5b18c4ae8832328
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 10:21:46 -0700

    Added POST to actions

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../data/helpers/actionModel.js');
+const projectDb = require('../data/helpers/projectModel.js');
 
 const router = express.Router();
 
@@ -31,5 +32,43 @@ router.get('/:id', (req, res) => {
             res.status(500).json({error: err, message: `The action with id ${id} could not be retrieved.`});
         });
 });
+
+router.post('/', validateActionBody, (req, res) => {
+    const newAction = req.body; 
+    db.insert(newAction)
+        .then(response => {
+            console.log(response);
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({message: "There was an error while adding the new action to the database", error: err});
+        });
+ });
+
+
+function validateActionBody(req, res, next) {
+    const newAction = req.body;
+    if (!newAction.project_id) {
+        res.status(400).json({error: "Please provide project_id for the action."});
+    } else if (!newAction.description) {
+        res.status(400).json({error: "Please provide a description for the action."});
+    } else if (!newAction.notes) {
+        res.status(400).json({error: "Please provide a notes object for the action."})
+    }
+    projectDb.get(newAction.project_id)
+        .then(response => {
+            console.log(response);
+            if (response) {
+               next();
+            } else {
+                res.status(404).json({message: `The project with id ${newAction.project_id} may not exist in the database`});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err, message: `The project with id ${newAction.project_id} could not be retrieved.`});
+        });
+};
 
 module.exports = router;
