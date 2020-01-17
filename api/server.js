c0bf5f78f46a3fd141467cb835990422435c48c6
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 10:47:33 -0700

    Added cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 
 const projectsRouter = require('../projects/projects-router.js');
 const actionsRouter = require('../actions/actions-router.js');
 
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
