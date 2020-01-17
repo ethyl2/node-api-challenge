6ada04ea16e1fea4b6020f6a4e88ea8b95b53f45
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 09:34:13 -0700

    GET by project id functionality added

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -15,4 +15,21 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db.get(id)
+        .then(response => {
+            console.log(response);
+            if (response) {
+                res.status(200).json(response);
+            } else {
+                res.status(404).json({message: `The project with id ${id} may not exist in the database`});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err, message: `The project with id ${id} could not be retrieved.`});
+        });
+});
+
 module.exports = router;
