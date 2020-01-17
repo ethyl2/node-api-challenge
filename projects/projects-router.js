98932c2c9f62dab872ffacb95c4f3b8f5e1c0ead
Heather Nuffer <zzzuzz@gmail.com>
2020-01-17 10:35:23 -0700

    Added validation middleware to check body in projects router

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -32,11 +32,8 @@ router.get('/:id', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateProjectBody, (req, res) => {
    const newProject = req.body;
-   if (!newProject.name || !newProject.description) {
-       res.status(400).json({error: "Please provide name and description for the project."});
-   } 
    db.insert(newProject)
     .then(response => {
         console.log(response);
@@ -49,12 +46,9 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:id', validateProjectId, (req, res) => {
+router.put('/:id', validateProjectId, validateProjectBody, (req, res) => {
     const id = req.params.id;
     const updatedProject = req.body;
-    if (!updatedProject.name || !updatedProject.description) {
-        res.status(400).json({error: `Please provide the name and description of the updated project of id ${id}`});
-    }
     db.update(id, updatedProject)
         .then(response => {
             console.log(response);
@@ -116,5 +110,15 @@ function validateProjectId(req, res, next) {
             res.status(500).json({error: err, message: `The project with id ${id} could not be retrieved.`});
         });
 };
+
+function validateProjectBody(req, res, next) {
+    const project = req.body;
+    if (!project.name) {
+        res.status(400).json({error: "Please provide name for the project."});
+    } else if (!project.description) {
+        res.status(400).json({error: "Please provide description for the project."});
+    }
+    next();
+}
 
 module.exports = router;
